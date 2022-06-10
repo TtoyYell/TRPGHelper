895df8d5b4f7d39541061e217c306ae63aab8a4e
Kanou <1255815114@qq.com>
2022-06-10 20:56:34 +0800

    Update 6/10

--- src/main/java/com/kanou/TrpgHelperApplication.java
+++ src/main/java/com/kanou/TrpgHelperApplication.java
@@ -1,17 +1,14 @@
 package com.kanou;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import java.util.Scanner;
-
 @SpringBootApplication
 public class TrpgHelperApplication {
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext applicationContext = SpringApplication.run(TrpgHelperApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TrpgHelperApplication.class, args);
+    }
 
 }
