1aab25650b27ee3b94b0e9766b48bfe90214ad80
Kanou <1255815114@qq.com>
2022-08-05 00:01:27 +0800

    跨域请求

--- src/main/java/com/kanou/controller/RoleController.java
+++ src/main/java/com/kanou/controller/RoleController.java
@@ -5,6 +5,7 @@ import com.kanou.entity.CocRole;
 import com.kanou.service.RoleService;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("/role")
+@CrossOrigin
 public class RoleController {
 
     @Autowired
